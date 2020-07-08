b51c7893eed8d5b74a18a3ec7a70873071cd5e28
Varun <varunsridharan23@gmail.com>
2020-07-08 11:31:37 +0530

    Added Basic Files.

---
+++ woocommerce-role-based-price.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name:       Role Based Price For WooCommerce
+ * Plugin URI:        https://wordpress.org/plugins/woocommerce-role-based-price/
+ * Description:       Sell product in different price for different user role based on your settings.
+ * Version:           4.0
+ * Author:            Varun Sridharan
+ * Author URI:        http://varunsridharan.in
+ * Text Domain:       wc-role-based-price
+ * Domain Path:       /i18n/
+ * WC requires at least: 3.0.0
+ * WC tested up to: 4.2
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * GitHub Plugin URI: https://github.com/varunsridharan/woocommerce-role-based-price/
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+defined( 'WC_RBP_WC_FILE' ) || define( 'WC_RBP_WC_FILE', __FILE__ );
+defined( 'WC_RBP_WC_VERSION' ) || define( 'WC_RBP_WC_VERSION', '4.0' );
+defined( 'WC_RBP_WC_NAME' ) || define( 'WC_RBP_WC_NAME', __( 'Role Based Price For WooCommerce' ) );
