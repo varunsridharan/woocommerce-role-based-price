1773c67b23079dd6536d8d1becaf3cbfa3d2a44a
Varun <varunsridharan23@gmail.com>
2020-07-10 21:19:06 +0530

    Created Addon Abstract.

---
+++ includes/class-addon.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace WC_RBP;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class Addon
+ *
+ * @package WC_RBP
+ * @author Varun Sridharan <varunsridharan23@gmail.com>
+ */
+abstract class Addon extends \VSP\Core\Abstracts\Addon {
+	/**
+	 * Returns WCRBP Plugin's Class Instance.
+	 *
+	 * @return \WC_RBP
+	 */
+	protected function plugin() {
+		return wc_rbp();
+	}
+}
