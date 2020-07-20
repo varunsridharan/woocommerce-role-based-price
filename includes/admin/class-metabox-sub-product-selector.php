584d6ba6ac3a0b8edbab2334fe6d58467b32fe40
Varun <varunsridharan23@gmail.com>
2020-07-20 08:46:59 +0530

    Added subproduct selectbox handler.

---
+++ includes/admin/class-metabox-sub-product-selector.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace WC_RBP\Admin;
+
+use VSP\Base;
+use VSP\WC_Compatibility;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class Metabox_Sub_Product_Selector
+ *
+ * @package WC_RBP\Admin
+ * @author Varun Sridharan <varunsridharan23@gmail.com>
+ */
+class Metabox_Sub_Product_Selector extends Base {
+	/**
+	 * Stores Product ID.
+	 *
+	 * @var string
+	 */
+	protected $product_id;
+
+	/**
+	 * Stores Sub Product ID.
+	 *
+	 * @var string
+	 */
+	protected $sub_product_id;
+
+	/**
+	 * Stores Selected Product.
+	 *
+	 * @var string
+	 */
+	protected $selected_product;
+
+	/**
+	 * Metabox_Sub_Product_Selector constructor.
+	 *
+	 * @param bool|string $product_id
+	 * @param bool|string $sub_product_id
+	 * @param bool|string $selected_product
+	 */
+	public function __construct( $product_id = false, $sub_product_id = false, $selected_product = false ) {
+		$this->product_id       = ( empty( $product_id ) ) ? wponion_get_var( 'wcrbp_product_id', false ) : $product_id;
+		$this->sub_product_id   = ( empty( $sub_product_id ) ) ? wponion_get_var( 'wcrbp_sub_product_id', false ) : $sub_product_id;
+		$this->selected_product = ( empty( $selected_product ) ) ? wponion_get_var( 'wcrbp_sub_product_selector', false ) : $selected_product;
+		$this->setup_variable();
+		$this->run_hook();
+	}
+
+	/**
+	 * Fetches Variation Products.
+	 */
+	protected function setup_variable() {
+		if ( 'variable' === WC_Compatibility::get_product_type( $this->product_id ) ) {
+			$this->settings = wponion_cast_array( $this->settings );
+			$products       = array();
+			/**
+			 * @var \WC_Product_Variable $product
+			 */
+			$product    = wc_get_product( $this->product_id );
+			$variations = $product->get_children();
+
+			foreach ( $variations as $variation ) {
+				$variation                        = wc_get_product( $variation );
+				$products[ $variation->get_id() ] = '#' . $variation->get_id() . ' ' . strip_tags( $variation->get_formatted_name() );
+			}
+			$this->settings ['variations'] = array(
+				'title'   => __( 'Variations' ),
+				'options' => $products,
+			);
+		}
+	}
+
+	/**
+	 * Triggers Hook And Generates Selectbox Data.
+	 */
+	protected function run_hook() {
+		/**
+		 * @param 1 settings array
+		 * @param 2 current product id
+		 * @param 3 current parent product id
+		 * @param 4 selected product type
+		 */
+		$this->settings = $this->apply_filter( 'metabox/sub_product_selector', $this->settings, $this->product_id, $this->sub_product_id, $this->selected_product );
+
+		if ( ! empty( $this->settings ) ) {
+			$select_html = array();
+			wc_rbp()->localizer()->add( 'metabox_sub_product_selector_raw', $this->settings, true, false );
+			foreach ( $this->settings as $slug => $values ) {
+				foreach ( $values['options'] as $id => $title ) {
+					if ( ! is_array( $title ) ) {
+						$title = array( 'label' => $title );
+					}
+					$title['attributes']                                 = ( wponion_is_set( $title, 'attributes' ) ) ? $title['attributes'] : array();
+					$title['attributes']['data-wcrbp-product-type-slug'] = $slug;
+					unset( $values['options'][ $id ] );
+					$values['options'][ $slug . '_' . $id ] = $title;
+				}
+				$select_html[ $values['title'] ] = $values['options'];
+			}
+
+			$html = wpo_field( 'select', 'wcrbp_sub_product_selector' )
+				->name( 'wcrbp_sub_product_selector' )
+				->attribute( 'id', 'wcrbp_sub_product_selector' )
+				->options( $select_html )
+				->only_field( true )
+				->render( $this->selected_product );
+			$html = '<div id="wcrbp-metabox-sub-product-selector" class="selectize-default">' . $html . '</div>';
+			wc_rbp()->localizer()->add( 'metabox_sub_product_selector_html', $html, false, false );
+		}
+	}
+}
