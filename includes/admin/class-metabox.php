a886ec7eb340e0263259cbeb63ae9fc74838f997
Varun <varunsridharan23@gmail.com>
2020-07-20 08:46:47 +0530

    Added Before Hook And some assets file.

--- includes/admin/class-metabox.php
+++ includes/admin/class-metabox.php
@@ -18,14 +18,22 @@ class Metabox extends Base {
 	 * Metabox constructor.
 	 */
 	public function __construct() {
-		$instance = new Price_Fields();
+		$this->add_action( 'wponion/metabox/render/_wc_role_based_price/before', 'before_hook', 10 );
+		$instance = wc_rbp()->_instance( '\WC_RBP\Admin\Price_Fields' );
 		wponion_metabox( array(
 			'metabox_id'    => 'role-based-price-editor',
 			'option_name'   => '_wc_role_based_price',
 			'save_type'     => '\WC_RBP\Admin\Save_Handler\WPO_Metabox',
 			'screens'       => 'product',
-			'assets'        => array( 'wcrbp-admin' ),
+			'assets'        => array( 'wcrbp-admin', 'selectize' ),
 			'metabox_title' => __( 'Role Based Price Editor' ),
 		), array( $instance, 'get' ) );
+	}
+
+	/**
+	 * @param int|string $post_id
+	 */
+	public function before_hook( $post_id ) {
+		wc_rbp()->_instance( '\WC_RBP\Admin\Metabox_Sub_Product_Selector', $post_id );
 	}
 }
